c154171df49eae52108025023b4c61d3f898cdbe
Tun Win Lat <tunwinlatt@windowslive.com>
2022-10-11 18:52:40 -0700

    Adding Login and Register Activities.

--- app/src/main/java/com/homecookapp/homecook/MainActivity.java
+++ app/src/main/java/com/homecookapp/homecook/MainActivity.java
@@ -2,7 +2,10 @@ package com.homecookapp.homecook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int name = 0;
+
+        Button btnRegister = findViewById(R.id.btnRegister);
+        Button btnLogin = findViewById(R.id.btnLogin);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LoadRegister();
+            }
+        });
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LoadLogin();
+            }
+        });
+    }
+
+    public void LoadRegister(){
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+    }
+
+    public void LoadLogin(){
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
