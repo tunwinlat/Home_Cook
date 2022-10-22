d70c6b1ad98331f9bbf84c3d280efdd9ca4d20f0
Tun Win Lat <tunwinlatt@windowslive.com>
2022-10-22 16:10:51 -0700

    Login configured with firebase connection.

--- app/src/main/java/com/homecookapp/homecook/LoginActivity.java
+++ app/src/main/java/com/homecookapp/homecook/LoginActivity.java
@@ -1,14 +1,100 @@
 package com.homecookapp.homecook;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.AuthResult;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText tvEmail, tvPassword;
+    private Button btnLogin;
+    //private ProgressBar pgBar;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        tvEmail = findViewById(R.id.txtEmail);
+        tvPassword = findViewById(R.id.txtPassword);
+        btnLogin = findViewById(R.id.btnLogin2);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                userLogin();
+            }
+        });
+    }
+
+    private void userLogin(){
+        String email = tvEmail.getText().toString();
+        String password = tvPassword.getText().toString();
+
+        if(TextUtils.isEmpty(email)){
+            Toast.makeText(getApplicationContext(),
+                            "Please enter email!!",
+                            Toast.LENGTH_LONG)
+                    .show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getApplicationContext(),
+                            "Please enter password!!",
+                            Toast.LENGTH_LONG)
+                    .show();
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(
+                        new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(
+                                    @NonNull Task<AuthResult> task)
+                            {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(getApplicationContext(),
+                                                    "Login successful!!",
+                                                    Toast.LENGTH_LONG)
+                                            .show();
+
+
+                                    Intent intent
+                                            = new Intent(LoginActivity.this,
+                                            HomeActivity.class);
+                                    startActivity(intent);
+                                }
+
+                                else {
+
+
+                                    Toast.makeText(getApplicationContext(),
+                                                    "Login failed!!",
+                                                    Toast.LENGTH_LONG)
+                                            .show();
+
+
+                                }
+                            }
+                        });
     }
 }
