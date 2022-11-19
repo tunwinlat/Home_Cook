4de93ffdba8d3f02bdfe5680e03dc76f03b93dea
alanlhy94 <115515042+alanlhy94@users.noreply.github.com>
2022-11-19 15:46:48 -0800

    Adding edit_product page and fixing string issues in add_product page

--- app/src/main/java/com/homecookapp/homecook/EditProduct_Activity.java
+++ app/src/main/java/com/homecookapp/homecook/EditProduct_Activity.java
@@ -1,4 +1,14 @@
 package com.homecookapp.homecook;
 
-public class EditProduct_Activity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditProduct_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_product);
+    }
 }
